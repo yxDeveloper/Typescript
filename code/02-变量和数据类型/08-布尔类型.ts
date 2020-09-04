ef201ba6918e109175177356c41e3bb355d489ff
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 18:43:38 +0800

    boolean type

---
+++ code/02-变量和数据类型/08-布尔类型.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:23
+ * @LastEditTime: 2020-09-04 18:42:51
+ * @LastEditors: Please set LastEditors
+ * @Description: boolean type
+ * @FilePath: \Typescript\code\02-变量和数据类型\08-布尔类型.ts
+ */
+// 真
+let isStudying: boolean = true
+// 假
+let isPlayingGame: boolean = false
+
+console.log(isStudying)
+console.log(isPlayingGame)
