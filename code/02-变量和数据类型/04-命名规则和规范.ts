6a22239fd1ca4fca8cdb998ae2d375a695545590
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 18:23:32 +0800

    命名规则

---
+++ code/02-变量和数据类型/04-命名规则和规范.ts
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:23
+ * @LastEditTime: 2020-09-04 18:23:06
+ * @LastEditors: Please set LastEditors
+ * @Description: 命名规则
+ * @FilePath: \Typescript\code\02-变量和数据类型\04-命名规则和规范.ts
+ */
+// 以下示例中变量名称是否正确：
+
+// let 2b 
+// let $name 
+// let first_name
+// let @email 
+
+let age1: number = 18
+let Age: number = 20
+console.log(age1)
+console.log(Age)
