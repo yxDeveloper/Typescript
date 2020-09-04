02dfc2948c16af28e643df05d88f0c761f5ac08c
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 19:13:43 +0800

    &&/||/!

---
+++ code/03-运算符/07-逻辑运算符.ts
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:24
+ * @LastEditTime: 2020-09-04 19:12:33
+ * @LastEditors: Please set LastEditors
+ * @Description: 逻辑运算符
+ * @FilePath: \Typescript\code\03-运算符\07-逻辑运算符.ts
+ */
+// 与：
+console.log(true && false)
+console.log(2 > 1 && 2 >= 2)
+
+// 或：
+console.log(true || false)
+console.log(3 < 2 || 1 >= 2)
+
+// 非：
+console.log(!true)
+console.log(!false || false)
