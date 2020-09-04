9d3303087139909a2ee7091e37949920bebe609c
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 18:14:22 +0800

    变量的基本使用

---
+++ code/02-变量和数据类型/01-变量的基本使用.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:23
+ * @LastEditTime: 2020-09-04 18:13:55
+ * @LastEditors: Please set LastEditors
+ * @Description: 变量的基本使用
+ * @FilePath: \Typescript\code\02-变量和数据类型\01-变量的基本使用.ts
+ */
+// 变量的基本使用：
+
+// // 第一步：声明变量并指定类型
+// let age: number
+
+// // 第二步：给变量赋值
+/* age = 18
+console.log(age) */
+
+// // 变量是可以变化的
+// age = 19
+// console.log(age)
+
+
+// 简化方式：声明变量的同时就赋值
+// let age: number = 20
+// console.log(age)
