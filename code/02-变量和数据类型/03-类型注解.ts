21474d7de1588c1e235565309d862d0c13363df8
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 18:20:14 +0800

    类型注解

--- code/02-变量和数据类型/03-类型注解.ts
+++ code/02-变量和数据类型/03-类型注解.ts
@@ -1,4 +1,13 @@
-let age: number = 18
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:23
+ * @LastEditTime: 2020-09-04 18:19:30
+ * @LastEditors: Please set LastEditors
+ * @Description: 类型注解
+ * @FilePath: \Typescript\code\02-变量和数据类型\03-类型注解.ts
+ */
+let age1: number = 18
 
 // 错误演示：
-age = 'Hello TS'
+// age1 = 'Hello TS'
+// 给变量赋值时不能赋类型不同的值
