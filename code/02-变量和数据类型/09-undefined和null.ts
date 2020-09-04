27b523709d3587c5165176703ab481262d9c072d
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 18:47:07 +0800

    typeof undefined and null

---
+++ code/02-变量和数据类型/09-undefined和null.ts
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:23
+ * @LastEditTime: 2020-09-04 18:46:06
+ * @LastEditors: Please set LastEditors
+ * @Description: undefind & null type
+ * @FilePath: \Typescript\code\02-变量和数据类型\09-undefined和null.ts
+ */
+// // undefined
+let u: undefined = undefined
+
+// null
+let n: null = null
+
+console.log(u)
+console.log(n)
+
+let u1: undefined
+console.log(u1)
