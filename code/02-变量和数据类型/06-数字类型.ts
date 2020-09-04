e6d1bce1f4155014c4a0ac8d6b97e28e62ef26b0
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 18:34:40 +0800

    number type

---
+++ code/02-变量和数据类型/06-数字类型.ts
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:23
+ * @LastEditTime: 2020-09-04 18:34:05
+ * @LastEditors: Please set LastEditors
+ * @Description: number
+ * @FilePath: \Typescript\code\02-变量和数据类型\06-数字类型.ts
+ */
+// 整数
+let size: number = 18
+
+// 小数
+let score: number = 11.2
+
+console.log(size)
+console.log(score)
+
+// 正数
+let salary: number = +2
+
+// 负数
+let salary_: number = -3
+
+console.log(salary)
+console.log(salary_)
+
+// let nul: number = null
+// let und: number = undefined
