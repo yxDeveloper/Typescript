6902691145c43756f5d1457414fc0dcf4272fe8b
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 18:36:18 +0800

    string type

---
+++ code/02-变量和数据类型/07-字符串类型.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:23
+ * @LastEditTime: 2020-09-04 18:35:29
+ * @LastEditors: Please set LastEditors
+ * @Description: string type
+ * @FilePath: \Typescript\code\02-变量和数据类型\07-字符串类型.ts
+ */
+console.log('Hello TS')
+console.log("Hello TS")
+
+// 声明字符串类型的变量
+let food: string = '糖葫芦'
+console.log(food)
