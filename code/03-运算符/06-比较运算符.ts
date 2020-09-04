04033d61893e185946783e06bdd8aa03726c5594
yxDeveloper <dilutian0415@gmail.com>
2020-09-04 19:09:45 +0800

    >/</===/!==/==

---
+++ code/03-运算符/06-比较运算符.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:24
+ * @LastEditTime: 2020-09-04 19:09:14
+ * @LastEditors: Please set LastEditors
+ * @Description: 比较运算符
+ * @FilePath: \Typescript\code\03-运算符\06-比较运算符.ts
+ */
+// // 大于
+// console.log(1 > 2)
+// // 大于等于
+// console.log(3 >= 2)
+// // 小于
+// console.log(1 < 2)
+// // 小于等于
+// console.log(3 <= 2)
+
+// 相等
+let num1: number = 3
+let num2: number = 4
+// console.log(3 === 4)
+console.log(num1 === num2)
+// 不相等
+// console.log(3 !== 4)
+console.log(num1 !== num2)
