1da63d5abdf961f5e08be5d422947c731b0abeb8
yxDeveloper <dilutian0415@gmail.com>
2020-09-05 18:21:11 +0800

    if

---
+++ code/4-语句/01-if语句.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:24
+ * @LastEditTime: 2020-09-05 18:20:46
+ * @LastEditors: Please set LastEditors
+ * @Description: if
+ * @FilePath: \Typescript\code\4-语句\01-if语句.ts
+ */
+// 变量isVip表示：是否为会员
+let isVip: boolean = true
+
+// 需求：如果是会员，就打印 '看完整电影'
+if (isVip) {
+  console.log('看完整电影')
+}
