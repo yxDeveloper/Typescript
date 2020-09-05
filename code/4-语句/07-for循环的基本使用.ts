bf2691ba6f9ca16a238eb4b56a15277b9e50505b
yxDeveloper <dilutian0415@gmail.com>
2020-09-05 18:32:45 +0800

    for

---
+++ code/4-语句/07-for循环的基本使用.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:24
+ * @LastEditTime: 2020-09-05 18:32:10
+ * @LastEditors: your name
+ * @Description: for
+ * @FilePath: \Typescript\code\4-语句\07-for循环的基本使用.ts
+ */
+// 在 TS 中，打印 3 遍以下内容：
+// 北冥有鱼，其名为鲲。鲲之大，一锅装不下
+// 
+// console.log('北冥有鱼，其名为鲲。鲲之大，一锅装不下')
+// console.log('北冥有鱼，其名为鲲。鲲之大，一锅装不下')
+// console.log('北冥有鱼，其名为鲲。鲲之大，一锅装不下')
+
+for (let i: number = 1; i <= 3; i++) {
+  console.log('北冥有鱼，其名为鲲。鲲之大，一锅装不下')
+}
