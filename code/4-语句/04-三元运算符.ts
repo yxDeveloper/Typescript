b885ea2105039d9447b87a0ce65ad97d9801c1e2
yxDeveloper <dilutian0415@gmail.com>
2020-09-05 18:28:27 +0800

    三元运算

---
+++ code/4-语句/04-三元运算符.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:24
+ * @LastEditTime: 2020-09-05 18:27:48
+ * @LastEditors: Please set LastEditors
+ * @Description: 三元运算符
+ * @FilePath: \Typescript\code\4-语句\04-三元运算符.ts
+ */
+// 判断 5 大于 3 吗？
+// 如果大于，就得到：'大于' ；否则，得到：'小于'
+let result: string = 5 > 3 ? '大于' : '小于'
+console.log(result)
