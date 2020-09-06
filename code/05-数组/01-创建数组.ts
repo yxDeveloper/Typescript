584254851f7754d869e91076953a2661796da13d
yxDeveloper <dilutian0415@gmail.com>
2020-09-06 18:58:01 +0800

    new array

---
+++ code/05-数组/01-创建数组.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:26
+ * @LastEditTime: 2020-09-06 18:57:24
+ * @LastEditors: Please set LastEditors
+ * @Description: 数组创建 array
+ * @FilePath: \Typescript\code\05-数组\01-创建数组.ts
+ */
+// 创建数组：
+// let names: string[] = ['迪丽热巴', '古力娜扎', '1']
+// console.log(names)
+let names1: string[] = []
+
+// 第二种方式：
+let names: string[] = new Array('迪丽热巴', '古力娜扎', '1')
+console.log(names)
+let names2: string[] = new Array('1')
