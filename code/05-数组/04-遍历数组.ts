d383145c8884f0ec100e2fe2ae17b615849783da
yxDeveloper <dilutian0415@gmail.com>
2020-09-06 19:01:13 +0800

    for array

---
+++ code/05-数组/04-遍历数组.ts
@@ -0,0 +1,13 @@
+// 遍历数组：
+
+let nums: number[] = [100, 200, 300, 400]
+
+// 使用 for 循环遍历数组，取出数组中的每一项元素
+// for (let i: number = 0; i <= nums.length - 1; i++) {
+//   console.log(nums[i])
+// }
+
+// 简化判断条件
+for (let i: number = 0; i < nums.length; i++) {
+  console.log(nums[i])
+}
