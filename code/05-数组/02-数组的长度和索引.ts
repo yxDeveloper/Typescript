f7e3e02322c7abec78d7241d342b0dc329d9aac4
yxDeveloper <dilutian0415@gmail.com>
2020-09-06 18:59:09 +0800

    array.length

---
+++ code/05-数组/02-数组的长度和索引.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:26
+ * @LastEditTime: 2020-09-06 18:58:39
+ * @LastEditors: Please set LastEditors
+ * @Description: length
+ * @FilePath: \Typescript\code\05-数组\02-数组的长度和索引.ts
+ */
+// 数组内容： 煎饼 馒头 米饭
+
+// 数组长度
+let foods: string[] = ['煎饼', '馒头', '米饭', '包子']
+console.log(foods.length)
+let food: string
