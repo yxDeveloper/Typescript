6908bf05f7cd6c626580d68fbca14dd1ae177479
yxDeveloper <dilutian0415@gmail.com>
2020-09-06 19:22:26 +0800

    function fn()

---
+++ code/06-函数基础/04-函数参数概述.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:27
+ * @LastEditTime: 2020-09-06 19:22:09
+ * @LastEditors: Please set LastEditors
+ * @Description: function fn(songName: string)
+ * @FilePath: \Typescript\code\06-函数基础\04-函数参数概述.ts
+ */
+// 唱歌的函数：
+// function sing() {
+//   console.log('五环之歌')
+// }
+
+// sing()
+// sing()
+
+function sing(songName: string) {
+  console.log(songName)
+}
+function sing1(songName: string) {
+  
+}
+
+sing('五环之歌')
+sing('探清水河')
