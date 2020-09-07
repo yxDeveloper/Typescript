a8801c2f3819303b16e5e97987661cda8fdadcfc
yxDeveloper <dilutian0415@gmail.com>
2020-09-07 20:45:55 +0800

    TS补充

---
+++ code/TS补充2.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-07 20:34:24
+ * @LastEditTime: 2020-09-07 20:45:13
+ * @LastEditors: Please set LastEditors
+ * @Description: TS补充
+ * @FilePath: \Typescript\code\TS补充2.ts
+ */
+const jianXiaoJieJie : ()=> string =()=>{return '大脚'}
+
+const zhaXiaoJieJie: () => void = () => {}
