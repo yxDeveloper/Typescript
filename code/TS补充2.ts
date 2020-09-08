e3804232e912f566ceeb7838dcd5d0a92cfe39d0
yxDeveloper <dilutian0415@gmail.com>
2020-09-09 00:07:41 +0800

    ts

--- code/TS补充2.ts
+++ code/TS补充2.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-07 20:34:24
- * @LastEditTime: 2020-09-07 20:45:13
+ * @LastEditTime: 2020-09-08 18:10:51
  * @LastEditors: Please set LastEditors
  * @Description: TS补充
  * @FilePath: \Typescript\code\TS补充2.ts
@@ -9,3 +9,18 @@
 const jianXiaoJieJie : ()=> string =()=>{return '大脚'}
 
 const zhaXiaoJieJie: () => void = () => {}
+
+let jiLaLa: (lala: string) => boolean = (lala: string) => {
+    return true
+}
+
+enum lalala {
+    la,
+    zh
+}
+interface lala {
+    la: string
+    ji: string
+}
+
+let state: lalala = lalala.la
