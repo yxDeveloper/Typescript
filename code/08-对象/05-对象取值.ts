a42fb44158250a28e24fa1ddc3b9b04e13151adb
yxDeveloper <dilutian0415@gmail.com>
2020-09-10 22:34:06 +0800

    object type

---
+++ code/08-对象/05-对象取值.ts
@@ -0,0 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:43
+ * @LastEditTime: 2020-09-10 18:03:15
+ * @LastEditors: Please set LastEditors
+ * @Description: Object type
+ * @FilePath: \Typescript\code\08-对象\05-对象取值.ts
+ */
+interface IUser {
+  name: string
+  height: number
+  sing: () => void
+}
+
+// 创建对象
+let jay1: IUser = {
+  name: '周杰伦',
+  height: 175,
+  sing: function () {
+    console.log('故事的小黄花。。。')
+  }
+}
+
+// 访问对象jay的name属性
+console.log(jay.name)
+console.log(jay.height)
+
+// 调用方法
+jay.sing()
+
+console.log('我叫', jay.name)
