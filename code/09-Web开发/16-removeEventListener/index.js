5a9224f29ed98a8321784c0ccef05430505b4fa3
yxDeveloper <dilutian0415@gmail.com>
2020-09-12 02:30:46 +0800

    removeEventListener

---
+++ code/09-Web开发/16-removeEventListener/index.js
@@ -0,0 +1,28 @@
+// 给 btn 添加事件
+var btn = document.querySelector('#btn');
+function handleClick() {
+    console.log('点我，你再点下试试！');
+}
+// btn.addEventListener('click', handleClick)
+// 演示事件只触发一次
+btn.addEventListener('click', handleClick, { once: true });
+// 点击移除事件按钮，移除 btn 的点击事件
+var removeBtn = document.querySelector('#remove');
+removeBtn.addEventListener('click', function () {
+    // 移除 btn 按钮的点击事件
+    btn.removeEventListener('click', handleClick);
+});
+// 错误演示：
+// // 给 btn 添加事件
+// let btn = document.querySelector('#btn') as HTMLButtonElement
+// btn.addEventListener('click', function () {
+//   console.log('点我，你再点下试试！')
+// })
+// // 点击移除事件按钮，移除 btn 的点击事件
+// let removeBtn = document.querySelector('#remove') as HTMLButtonElement
+// removeBtn.addEventListener('click', function () {
+//   // 移除 btn 按钮的点击事件
+//   btn.removeEventListener('click', function () {
+//     console.log('点我，你再点下试试！')
+//   })
+// })
