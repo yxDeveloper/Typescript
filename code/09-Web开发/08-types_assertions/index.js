855c3e164a0a22535e1016907472e36e10062af1
yxDeveloper <dilutian0415@gmail.com>
2020-09-12 01:56:35 +0800

    types_assertions

---
+++ code/09-Web开发/08-types_assertions/index.js
@@ -0,0 +1,10 @@
+// 获取 h1 元素：
+var title = document.querySelector('#title');
+// title.id
+console.log(title);
+console.dir(title);
+// 获取 img 元素：
+var img = document.querySelector('#image');
+img.src = './2.jpg';
+// img.id
+console.dir(img);
