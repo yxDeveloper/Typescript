d432c4bbac9845a9b0f9076c0a5593f2b59f8f2a
yxDeveloper <dilutian0415@gmail.com>
2020-09-12 01:49:23 +0800

    querySelector

---
+++ code/09-Web开发/07-querySelector/index.js
@@ -0,0 +1,7 @@
+// 示例： 获取 id 为 title 的 DOM 元素
+// let title = document.querySelector('#title')
+// console.log(title)
+// let title = document.querySelector('p')
+// console.log(title)
+var title = document.querySelector('.c1');
+console.log(title);
