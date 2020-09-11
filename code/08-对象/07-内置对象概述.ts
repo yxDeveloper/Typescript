40a147ae5c73c6803e7637f7e563e1bf7fdc1ce6
yxDeveloper <dilutian0415@gmail.com>
2020-09-12 00:17:45 +0800

    Object 内置对象 Math

--- code/08-对象/07-内置对象概述.ts
+++ code/08-对象/07-内置对象概述.ts
@@ -1,2 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 17:59:43
+ * @LastEditTime: 2020-09-12 00:17:06
+ * @LastEditors: Please set LastEditors
+ * @Description: 内置对象 Math
+ * @FilePath: \Typescript\code\08-对象\07-内置对象概述.ts
+ */
 console.log(Math.max(1, 3, 2));
 // expected output: 3
+console.log(Math.min(1, 2, 4));
+
