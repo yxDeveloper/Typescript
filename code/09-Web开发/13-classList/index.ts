90341904615f21770dc41866c7f020d560b6f459
yxDeveloper <dilutian0415@gmail.com>
2020-09-12 02:18:46 +0800

    classList remove add contains

---
+++ code/09-Web开发/13-classList/index.ts
@@ -0,0 +1,15 @@
+// p元素
+let p = document.querySelector('#txt') as HTMLParagraphElement
+
+// 添加类
+p.classList.add('b', 'c')
+// p.classList.add
+// p.classList.remove
+// p.classList.contains
+
+// 移除类
+p.classList.remove('b', 'c')
+
+// 判断类是否存在
+let has = p.classList.contains('b')
+console.log(has)
