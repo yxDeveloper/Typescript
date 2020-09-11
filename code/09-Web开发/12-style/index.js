7f57076eb42e679d024424ee332f4ae40b0f8fac
yxDeveloper <dilutian0415@gmail.com>
2020-09-12 02:16:43 +0800

    style

---
+++ code/09-Web开发/12-style/index.js
@@ -0,0 +1,12 @@
+// p元素
+var p = document.querySelector('#txt');
+// 读取：
+console.log(p.style.fontSize);
+// 设置：
+p.style.fontSize = '30px';
+p.style.color = 'red';
+// 隐藏当前元素
+p.style.display = 'none';
+// 展示p元素
+p.style.display = 'block';
+console.log(p.style.fontSize);
