3b3c02db4373aec70da2e11e0c164741f58d3744
yxDeveloper <dilutian0415@gmail.com>
2020-09-12 00:42:07 +0800

    数组内置对象forEach

--- code/08-对象/09-数组对象2（forEach）.ts
+++ code/08-对象/09-数组对象2（forEach）.ts
@@ -1,13 +1,13 @@
-let songs: string[] = ['五环之歌', '探清水河', '晴天']
+let songs2: Array<string> = ['五环之歌', '探清水河', '晴天']
 
 // 使用for循环，遍历数组：
-for (let i: number = 0; i < songs.length; i++) {
-  console.log('索引为', i, '元素为', songs[i])
+for (let i: number = 0; i < songs2.length; i++) {
+  console.log('索引为', i, '元素为', songs2[i])
 }
 
 // 使用forEach方法，遍历数组：
-songs.forEach(function (item, index) {
-  console.log('索引为', index, '元素为', item)
+songs2.forEach((item, i) => {
+  console.log('索引为', i, '元素为', item)
 })
 
 // 3:10
