d83dafe2f62fcac801d8b9b2033b03ff6b82f3e9
yxDeveloper <dilutian0415@gmail.com>
2020-09-16 23:33:11 +0800

    String[] type

--- code/TS补充2.ts
+++ code/TS补充2.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-07 20:34:24
- * @LastEditTime: 2020-09-08 18:10:51
+ * @LastEditTime: 2020-09-16 23:32:16
  * @LastEditors: Please set LastEditors
  * @Description: TS补充
  * @FilePath: \Typescript\code\TS补充2.ts
@@ -24,3 +24,10 @@ interface lala {
 }
 
 let state: lalala = lalala.la
+
+// const XiaoJieJie: String[] = ['1','2','3']
+const XiaoJieJie: string[] = ['1','2','true']
+// error: const XiaoJieJie: string[] = ['1','2',true]
+
+
+console.log(XiaoJieJie);
